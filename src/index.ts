511343ed6f1ca412b691d5faab2a9f41f47c1070
artemsr <artem.serebryakov@nih.gov>
2020-06-03 15:09:26 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {ComputeApplication} from './application';
+import {ExpressServer} from './server';
+export {ComputeApplication};
+export {ComputeApplication as app};
+require('dotenv').config();
+const config = require('config');
+
+export async function main() {
+  const server = new ExpressServer(config);
+  await server.boot();
+  await server.start();
+  console.log(
+    `Server is running at http://127.0.0.1:${config.rest.port}${config.compute.basePath}`,
+  );
+}
---
+++ src/keys.ts
@@ -0,0 +1,5 @@
+import {BindingKey} from '@loopback/context';
+export namespace ComputeApiBindings {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const CONFIG = BindingKey.create<any>('Compute.config');
+}
