2278712a845838f97502f2c05af1114171ee4aa6
artemsr <artem.serebryakov@nih.gov>
2020-06-05 15:42:32 -0400

    feat: pass config unti driver

--- src/shared/driver.ts
+++ src/shared/driver.ts
@@ -1,3 +1,8 @@
 export interface Driver {
   getType(): string;
 }
+
+export abstract class BaseDriver implements Driver {
+  constructor(n: string) { }
+  abstract getType(): string;
+}
--- src/test-drivers/driver-one.ts
+++ src/test-drivers/driver-one.ts
@@ -1,7 +1,12 @@
-import { Driver } from '../shared/driver';
+import { BaseDriver } from '../shared/driver';
 
-export class DriverOne implements Driver {
-  private type = 'This is Driver One';
+export class DriverOne extends BaseDriver {
+  private type = 'default driver one';
+
+  constructor(n: string) {
+    super(n);
+    this.type = n;
+  }
 
   getType(): string {
     return this.type;
