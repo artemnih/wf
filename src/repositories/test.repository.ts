758ad0cde75fd23cea6b9238fce77f63721efa9e
artemsr <artem.serebryakov@nih.gov>
2020-06-08 22:07:35 -0400

    fix(test): removed config injection

--- src/repositories/test.repository.ts
+++ src/repositories/test.repository.ts
@@ -1,12 +1,9 @@
-import { inject, ApplicationConfig } from '@loopback/core';
+import { inject } from '@loopback/core';
 import { ComputeApiBindings } from '../keys';
 import { DriverFactory } from '../factories/driver.factory';
 
 export class TestRepository {
-  constructor(
-    @inject(ComputeApiBindings.DRVIER_FACTORY) private driverFactory: DriverFactory,
-    @inject(ComputeApiBindings.CONFIG) private config: ApplicationConfig,
-  ) {}
+  constructor(@inject(ComputeApiBindings.DRVIER_FACTORY) private driverFactory: DriverFactory) {}
 
   public getType(manifestName: string) {
     const driver = this.driverFactory.getInstance(manifestName);
