0e9dc4d97312993edf6fd400628dfced0e420e1b
artemnih <23387542+artemnih@users.noreply.github.com>
2020-06-16 13:09:25 -0400

    feat(service-config): updated model

--- src/models/service-config.model.ts
+++ src/models/service-config.model.ts
@@ -1,29 +1,30 @@
 import { Entity, model, property } from '@loopback/repository';
 
 @model({
-  name: 'drivers',
-  settings: { strict: false },
+    name: 'drivers',
+    settings: { strict: false },
 })
 export class ServiceConfig extends Entity {
-  @property({
-    type: 'number',
-    id: true,
-    generated: true,
-  })
-  id?: string;
+    @property({ type: 'number', id: true, generated: true, }) id?: string;
 
-  @property({
-    type: 'string',
-  })
-  title?: string;
+    @property({ type: 'string' }) title: string;
 
-  constructor(data?: Partial<ServiceConfig>) {
-    super(data);
-  }
+    @property({ type: 'string' }) url: string;
+
+    @property({ type: 'string' }) port: string;
+
+    @property({ type: 'string' }) user: string;
+
+    @property({ type: 'string' }) password: string;
+
+
+    constructor(data?: Partial<ServiceConfig>) {
+        super(data);
+    }
 }
 
 export interface ServiceConfigRelations {
-  // describe navigational properties here
+    // describe navigational properties here
 }
 
 export type ServiceConfigWithRelations = ServiceConfig & ServiceConfigRelations;
