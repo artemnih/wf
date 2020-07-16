97beef0d70c2b27c3ae6bca225a81c61fabf5107
artemnih <23387542+artemnih@users.noreply.github.com>
2020-07-16 17:37:36 -0400

    feat: added compute repository

---
+++ src/repositories/compute.repository.ts
@@ -0,0 +1,22 @@
+import { inject } from '@loopback/core';
+import { ComputeApiBindings } from '../keys';
+import { DriverFactory } from '../factories/driver.factory';
+import { ServiceConfig } from '../models/service-config.model';
+
+export class ComputeRepository {
+  constructor(
+    @inject(ComputeApiBindings.DRVIER_FACTORY) private driverFactory: DriverFactory
+  ) { }
+
+  public async compute(serviceConfig: ServiceConfig, script: string) {
+    const driver = await this.driverFactory.getInstance(serviceConfig);
+    console.log('computing.....')
+    driver.compute(script);
+  }
+
+  public async install(serviceConfig: ServiceConfig) {
+    console.log('installing.....');
+    const driver = await this.driverFactory.install(serviceConfig.package);
+  }
+
+}
