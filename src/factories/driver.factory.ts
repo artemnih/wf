b917cea8709a069985ba508b3cbcd341f93731e2
artemnih <23387542+artemnih@users.noreply.github.com>
2020-07-24 13:32:37 -0400

    feat: install driver in runtime on attemp to run script

--- src/factories/driver.factory.ts
+++ src/factories/driver.factory.ts
@@ -1,12 +1,30 @@
-import { PackageManager } from '../utils/package-manager'; // uncomment if using dynamic npm install
+import { PackageManager } from '../utils/package-manager';
 import { ServiceConfig } from '../models/service-config.model';
 import { Driver } from '../types/driver';
 
 export class DriverFactory {
-  public async getInstance(serviceConfig: ServiceConfig) {
-    // const DriverClass = (await import('../drivers/microservice-driver')).default;
-    const DriverClass = (await import(serviceConfig.package)).default;
-    return new DriverClass(serviceConfig.config) as Driver;
+
+  public async getInstance(serviceConfig: ServiceConfig, allowInstallAndRetry = true): Promise<Driver | undefined> {
+    const packageName = serviceConfig.package;
+    try {
+      const DriverClass = (await import(packageName)).default;
+      return new DriverClass(serviceConfig.config) as Driver;
+    } catch (e) {
+      if (e.message === `Cannot find module '${packageName}'`) {
+        console.log('Driver is not installed');
+        if (allowInstallAndRetry) {
+          console.log('Attempting to install the driver and try again');
+          
+          await this.install(packageName);
+          console.log('Get driver again');
+
+          // eslint-disable-next-line no-return-await
+          return await this.getInstance(serviceConfig, false);
+        } else {
+          console.log('Failed to get drivers');
+        }
+      }
+    }
   }
 
   public async install(packageName: string) {
--- src/repositories/compute.repository.ts
+++ src/repositories/compute.repository.ts
@@ -4,11 +4,13 @@ import { DriverFactory } from '../factories/driver.factory';
 import { ServiceConfig } from '../models/service-config.model';
 
 export class ComputeRepository {
-  constructor(@inject(ComputeApiBindings.DRVIER_FACTORY) private driverFactory: DriverFactory) {}
+  constructor(@inject(ComputeApiBindings.DRVIER_FACTORY) private driverFactory: DriverFactory) { }
 
   public async compute(serviceConfig: ServiceConfig, script: string) {
     const driver = await this.driverFactory.getInstance(serviceConfig);
-    await driver.compute(script);
+    if (driver) {
+      await driver.compute(script);
+    }
   }
 
   public async install(serviceConfig: ServiceConfig) {
