2a15f4d663c73a794f0dae1435cc819ec3164f9d
artemnih <23387542+artemnih@users.noreply.github.com>
2023-09-05 17:13:42 -0400

    updated file import

--- packages/api/src/drivers/index.ts
+++ packages/api/src/drivers/index.ts
@@ -1,5 +1,5 @@
 export * from './driver-argo';
-export * from './driver-base';
+export * from './driver-factory';
 export * from './driver-slurm';
 export * from './common';
 export * from './driver'
