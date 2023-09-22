83d9f5538f2f6871f6b21de89befc09e0aed33ab
artemnih <23387542+artemnih@users.noreply.github.com>
2023-09-22 14:31:04 -0400

    Single Node Driver (#17)
    * removed readme
    
    * removed old commands
    
    * added argo driver package
    
    * updated deps, removed lerna. loopback
    
    * remove old assets
    
    * updated code
    
    * deleted readme
    
    * deleted old configs
    
    * updated tsconfig
    
    * deleted loopback assets
    
    * remove old configs
    
    * format
    
    * added slurm pacakge
    
    * update deps and scripts
    
    * replace Loopback with Express
    
    * single node driver
    
    * uncomment auth
    
    * PR review changes

---
+++ packages/single-node-driver/README.md
@@ -0,0 +1,16 @@
+## Pre-requisites
+-`python 3.6+`
+-`pip3`
+- `node` version 16.0.0 or higher
+- Make sure `cwltool` is installed and available in the path
+
+```bash
+echo "export PATH=\"`python3 -m site --user-base`/bin:\$PATH\"" >> ~/.bashrc
+source ~/.bashrc
+```
+
+## Installation
+1. `npm install`
+
+## Running
+2. `npm run dev`
---
+++ packages/single-node-driver/src/server.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as http from 'http';
+import cors from "cors";
+import jwksRsa from "jwks-rsa";
+import { expressjwt } from 'express-jwt';
+import { WorkflowRoutes, HealthRoutes } from './router';
+
+export class ExpressServer {
+  private app: express.Application;
+  private server: http.Server;
+
+  constructor(private options: any) {
+    const authUrl = this.options.services.auth.authUrl;
+ 
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use('/compute',
+      expressjwt({
+        secret: jwksRsa.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `${authUrl}/.well-known/jwks.json`,
+        }) as any,
+        algorithms: ['RS256'],
+        issuer: authUrl,
+      })
+    );
+
+    // Expose the front-end assets via Express, not as LB4 route
+    // this.app.use(this.options.singleNodeCompute.basePath, this.api.requestHandler);
+
+    this.app.use("/compute", WorkflowRoutes);
+    this.app.use("/health", HealthRoutes);
+    
+    // Serve static files in the public folder
+    this.app.use(express.static('public'));
+  }
+
+  public async start() {
+    this.server = this.app.listen(this.options.rest.port, this.options.rest.host);
+  }
+
+  public async stop() {
+    if (!this.server) return;
+    this.server.close();
+  }
+}
