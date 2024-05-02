a4be67c402ae8be3fcd8b29d5d6e2cf946c95024
artemnih <23387542+artemnih@users.noreply.github.com>
2024-05-02 15:53:46 -0400

    fix all logs

--- packages/argo-driver/src/services/argoApi/get-all-jobs-logs.ts
+++ packages/argo-driver/src/services/argoApi/get-all-jobs-logs.ts
@@ -7,7 +7,7 @@ export async function getAllJobsLogs(workflowId: string) {
 	const workflow = (await axiosClient().get(`/${workflowId}`)) as ArgoJobStatus;
 	const pods = Object.values(workflow.data.status.nodes).filter(value => value.type === 'Pod') as Array<ArgoNodes>;
 	const podNames = pods.map(pod => pod.id);
-	const urls = podNames.map(podName => `/${workflowId}/log?logOptions.container=wait&logOptions.follow=true&podName=${podName}`);
+	const urls = podNames.map(podName => `/${workflowId}/log?logOptions.container=wait&podName=${podName}`);
 	const promises = urls.map(url => axiosClient().get(url));
 	const results = await Promise.all(promises);
 	const datas = results.map(result => result.data);
