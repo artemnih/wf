417a64c5647975a38fd4c25e3afa146306e71ca5
JesseMckinzie <jessemckinzie145@gmail.com>
2024-08-20 07:08:37 -0600

    Clean up SlurmController

--- packages/slurm-driver/src/controllers/slurm.controller2.ts
+++ packages/slurm-driver/src/controllers/slurm.controller2.ts
@@ -1,10 +1,11 @@
 import { Slurm2 } from '../models';
 import SlurmRepository from '../repositories/slurm.repository';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express'; 
 import { HpcCli, SlurmCli, toilKillHandler } from '../hpc';
 const multer = require("multer");
 const path = require("path");
 var fs = require('fs');
+const slurmConfig = require('config');
 
 class SlurmController2 {
 
@@ -12,6 +13,7 @@ class SlurmController2 {
 
 		try {
 			
+			// parse request
 			const slurm = req.body as Slurm2;
 
 			// ensure a CWL workflow is provided
@@ -31,21 +33,12 @@ class SlurmController2 {
 			
 			// write inputs for toil
 			fs.writeFileSync('command.json', JSON.stringify(slurm.cwlJobInputs));
-			
-			// check for custom configuration
-			var config: string[]
-			if (slurm.config)  {
-				config = slurm.config
-			}
 
-			var workflowId: string;
+			// check for custom configuration
+			const config: string[] = slurm.config ?? [];
 
 			// check if workflow id was supplied
-			if (slurm.id) {
-				workflowId = slurm.id;
-			} else { // set to CWL id if not
-				workflowId = JSON.parse(JSON.stringify(slurm.cwlWorkflow)).id;
-			}
+			const workflowId: string = slurm.id ? slurm.id : JSON.parse(JSON.stringify(slurm.cwlWorkflow)).id;;
 
 			const result = SlurmRepository.computeCwlFile('cwl.json', 'command.json', workflowId, config);
 			
@@ -70,21 +63,99 @@ class SlurmController2 {
 	}
 
 	async getWorkflowLogs(req: Request, res: Response, next: NextFunction) {
+		// get workflow id
+		const id = req.params.id;
+
+		// working directory for toil
+		var currentDir = slurmConfig.slurmCompute.data
+
 		try {
-			const id = req.params.id;
-			const result = SlurmRepository.getWorkflowLogs(id);
-			res.status(201).json(result);
-		} catch (error) {
-			next(error);
+			
+			// read contents from correct directory for the workflow id
+			const outputPath = `${currentDir}/${id}/logs`
+			const fileList = fs.readdirSync(outputPath);
+
+			var result: JSON;
+			var temp: any = {};
+
+			// read all files in the output directory and send as JSON
+			fileList.forEach((file: any) => {
+
+				try {
+
+					// read contents of file and add to JSON object with key as the filename
+					temp[file] = fs.readFileSync(`${outputPath}/${file}`, 'utf-8');
+
+				} catch (err) {
+
+					console.error(`Error reading ${file}:`, err);
+
+					res.status(500);
+					res.send(`Error reading ${file} for workflow id ${id}.`);
+				}
+			});
+
+		} catch (err) {
+			console.error(err);
+
+			res.status(404);
+			res.send(`Invalid workflow id ${id}. Make sure that id=${id} is a valid workflow`);
 		}
+
+		// cast results to JSON
+		result = <JSON>temp;
+
+		// send contents of file
+		res.status(200).json(result);
 	}
 
 	async getWorkflowOutputs(req: Request, res: Response, next: NextFunction) {
 		try {
+
+			// get workflow id
 			const id = req.params.id;
-			console.log("id: " + id);
-			const result = SlurmRepository.getWorkflowOutputs(id);
-			res.status(201).json(result);
+
+			// working directory for toil
+			var currentDir = slurmConfig.slurmCompute.data
+
+			try {
+				
+				// read contents from correct directory for the workflow id
+				const outputPath = `${currentDir}/${id}/out`
+				const fileList = fs.readdirSync(outputPath);
+
+				var result: JSON;
+				var temp: any = {};
+
+				// read all files in the output directory and send as JSON
+				fileList.forEach((file: any) => {
+
+					try {
+
+						// read contents of file and add to JSON object with key as the filename
+						temp[file] = fs.readFileSync(`${outputPath}/${file}`, 'utf-8');
+
+					} catch (err) {
+
+						console.error(`Error reading ${file}:`, err);
+
+						res.status(500);
+						res.send(`Error reading ${file} for workflow id ${id}.`);
+					}
+				});
+
+			} catch (err) {
+				console.error(err);
+
+				res.status(404);
+				res.send(`Invalid workflow id ${id}. Make sure that id=${id} is a valid workflow`);
+			}
+
+			// cast results to JSON
+			result = <JSON>temp;
+
+			// send contents of file
+			res.status(200).json(result);
 		} catch (error) {
 			next(error);
 		}
