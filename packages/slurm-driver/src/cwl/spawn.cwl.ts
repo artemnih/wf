e3124d8ddd981c923cc040be65d43bf6671524d4
JesseMckinzie <jessemckinzie145@gmail.com>
2024-08-23 12:21:45 -0600

    Remove second spawnGenericCwlRunner

--- packages/slurm-driver/src/cwl/spawn.cwl.ts
+++ packages/slurm-driver/src/cwl/spawn.cwl.ts
@@ -2,34 +2,7 @@ import { ChildProcess, spawn } from 'child_process';
 import { mkdir, mkdirSync } from 'fs';
 var fs = require('fs');
 
-export function spawnGenericCwlRunner(cwlWorkflow: string, cwlJobInputs: string, currentDir: string, workflowId: string): ChildProcess {
-	return spawn(
-		'toil-cwl-runner',
-		[
-			'--singularity',
-			'--dont_allocate_mem',
-			'--bypass-file-store',
-			'--jobStore',
-			`${currentDir}/${workflowId}`,
-			'--tmpdir-prefix',
-			`${currentDir}/${workflowId}`,
-			'--tmp-outdir-prefix',
-			`${currentDir}/${workflowId}`,
-			'--batchSystem',
-			'slurm',
-			'--log-dir',
-			`${currentDir}/${workflowId}-logs`,
-			'--disableCaching',
-			cwlWorkflow,
-			cwlJobInputs,
-		],
-		{
-			cwd: currentDir,
-		},
-	);
-}
-
-export function spawnGenericCwlRunner2(
+export function spawnGenericCwlRunner(
 	cwlWorkflow: string,
 	cwlJobInputs: string,
 	currentDir: string,
