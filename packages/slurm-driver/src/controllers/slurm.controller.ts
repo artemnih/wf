b601f17dd3470727f398bc580b692a7250c07188
JesseMckinzie <jessemckinzie145@gmail.com>
2024-08-23 12:13:15 -0600

    Update slurm controller with new functionality from compute2 endpoint

--- packages/slurm-driver/src/controllers/slurm.controller.ts
+++ packages/slurm-driver/src/controllers/slurm.controller.ts
@@ -2,12 +2,45 @@ import { Slurm } from '../models';
 import SlurmRepository from '../repositories/slurm.repository';
 import { NextFunction, Request, Response } from 'express';
 import { HpcCli, SlurmCli, toilKillHandler } from '../hpc';
+import { IControllerController } from '@polusai/compute-common';
 
-class SlurmController {
-	async create(req: Request, res: Response, next: NextFunction) {
+var fs = require('fs');
+const slurmConfig = require('config');
+
+class SlurmController implements IControllerController {
+	async createWorkflow(req: Request, res: Response, next: NextFunction) {
 		try {
+			// parse request
 			const slurm = req.body as Slurm;
-			const result = SlurmRepository.compute(slurm.cwlWorkflow, slurm.cwlJobInputs, slurm.jobs);
+			
+			console.log(slurm);
+			// ensure a CWL workflow is provided
+			if (!slurm.cwlWorkflow) {
+				res.status(400);
+				res.send('Key "cwlWorkflow" not found in request. A valid CWL workflow must be provided');
+			}
+
+			// store CWL to pass to toil
+			fs.writeFileSync('cwl.json', JSON.stringify(slurm.cwlWorkflow));
+
+			// ensure job inputs for CWL is provided
+			if (!slurm.cwlJobInputs) {
+				res.status(400);
+				res.send('Key "cwlJobInputs" not found in request. An input for the CWL job must be included.');
+			}
+
+			// write inputs for toil
+			fs.writeFileSync('command.json', JSON.stringify(slurm.cwlJobInputs));
+
+			// check for custom configuration
+			const config: string[] = slurm.config ?? [];
+
+			// check if workflow id was supplied
+			const workflowId: string = slurm.id ? slurm.id : JSON.parse(JSON.stringify(slurm.cwlWorkflow)).id;
+
+			const result = SlurmRepository.computeCwlFile('cwl.json', 'command.json', workflowId, config);
+
+			// return status that workflow is has started
 			res.status(201).json(result);
 		} catch (error) {
 			next(error);
@@ -28,20 +61,86 @@ class SlurmController {
 	}
 
 	async getWorkflowLogs(req: Request, res: Response, next: NextFunction) {
+		// get workflow id
+		const id = req.params.id;
+
+		// working directory for toil
+		var currentDir = slurmConfig.slurmCompute.data;
+
 		try {
-			const id = req.params.id;
-			const result = SlurmRepository.getWorkflowLogs(id);
-			res.status(201).json(result);
-		} catch (error) {
-			next(error);
+			// read contents from correct directory for the workflow id
+			const outputPath = `${currentDir}/${id}/logs`;
+			const fileList = fs.readdirSync(outputPath);
+
+			var result: JSON;
+			var temp: any = {};
+
+			// read all files in the output directory and send as JSON
+			fileList.forEach((file: any) => {
+				try {
+					// read contents of file and add to JSON object with key as the filename
+					temp[file] = fs.readFileSync(`${outputPath}/${file}`, 'utf-8');
+				} catch (err) {
+					console.error(`Error reading ${file}:`, err);
+
+					res.status(500);
+					res.send(`Error reading ${file} for workflow id ${id}.`);
+				}
+			});
+		} catch (err) {
+			console.error(err);
+
+			res.status(404);
+			res.send(`Invalid workflow id ${id}. Make sure that id=${id} is a valid workflow`);
 		}
+
+		// cast results to JSON
+		result = <JSON>temp;
+
+		// send contents of file
+		res.status(200).json(result);
 	}
 
 	async getWorkflowOutputs(req: Request, res: Response, next: NextFunction) {
 		try {
+			// get workflow id
 			const id = req.params.id;
-			const result = SlurmRepository.getWorkflowOutputs(id);
-			res.status(201).json(result);
+
+			// working directory for toil
+			var currentDir = slurmConfig.slurmCompute.data;
+
+			try {
+				// read contents from correct directory for the workflow id
+				const outputPath = `${currentDir}/${id}/out`;
+				const fileList = fs.readdirSync(outputPath);
+
+				var result: JSON;
+				var temp: any = {};
+
+				// read all files in the output directory and send as JSON
+				fileList.forEach((file: any) => {
+					try {
+						// read contents of file and add to JSON object with key as the filename
+						temp[file] = fs.readFileSync(`${outputPath}/${file}`, 'utf-8');
+					} catch (err) {
+						console.error(`Error reading ${file}:`, err);
+
+						res.status(500);
+						res.send(`Error reading ${file} for workflow id ${id}.`);
+					}
+				});
+			} catch (err) {
+				console.error(err);
+
+				res.status(404);
+				res.send(`Invalid workflow id ${id}. Make sure that id=${id} is a valid workflow`);
+			}
+
+			// cast results to JSON
+			result = <JSON>temp;
+
+			// send contents of file
+			res.status(200).json(result);
 		} catch (error) {
 			next(error);
 		}
@@ -58,7 +157,7 @@ class SlurmController {
 		}
 	}
 
-	stopWorkflow(req: Request, res: Response, next: NextFunction) {
+	async stopWorkflow(req: Request, res: Response, next: NextFunction) {
 		try {
 			const id = req.params.id;
 			const hpcCli: HpcCli = new SlurmCli();
@@ -68,6 +167,18 @@ class SlurmController {
 		} catch (error) {
 			next(error);
 		}
+	}
+
+	async getAllJobsLogs(req: Request, res: Response, next: NextFunction) {
+		res.status(501).send("Job logs are not implemented for the Slurm driver");
+	}
+
+	async getJobLogs(req: Request, res: Response, next: NextFunction) {
+		res.status(501).send("Job logs are not implemented for the Slurm driver");
+	}
+
+	async getContent(req: Request, res: Response, next: NextFunction) {
+		res.status(501).send("Job logs are not implemented for the Slurm driver");
 	}
 }
 
